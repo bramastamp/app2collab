ed35ed425ecefaf4ddace76c947308c21a13ca05
Galang6sa <galangsur9@gmail.com>
2025-07-30 11:17:34 +0700

    add splsh

--- app/src/main/java/com/smkth/app2_colab/MainActivity.kt
+++ app/src/main/java/com/smkth/app2_colab/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.smkth.app2_colab
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var inputName: EditText
+    private lateinit var inputKelas: EditText
+    private lateinit var btnSubmit: Button
+    private lateinit var txtResult: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,18 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        inputName = findViewById(R.id.etNama)
+        inputKelas = findViewById(R.id.etKelas)
+        btnSubmit = findViewById(R.id.btnTampilkan)
+        txtResult = findViewById(R.id.tvHasil)
+
+        btnSubmit.setOnClickListener {
+            val nama = inputName.text.toString().trim()
+            var kelas = inputKelas.text.toString().trim()
+            var hasil = "Nama: $nama\nKelas: $kelas"
+            txtResult.text = hasil
         }
     }
 }
